6856671239fe8a9e7d6c3aca917dc1711a13b630
FarhadMohamad <art_ismat@hotmail.com>
2020-02-26 15:07:54 +0100

    First change

---
+++ MitBud/Providers/ClientProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MitBud.DAL;
+using MitBud.Models;
+
+namespace MitBud.Providers
+{
+    public class ClientProvider
+    {
+        public static void SaveClientInfo(RegisterBindingModel registerViewModel, string UserId)
+        {
+            MitBudDBEntities db = new MitBudDBEntities();
+            Client client = new Client();
+            client.Client_Id = UserId;
+            client.Name = registerViewModel.Name;
+            client.Email = registerViewModel.Email;
+          
+            db.Clients.Add(client);
+            db.SaveChanges();
+
+
+        }
+    }
+}
---
+++ MitBud/Providers/TaskProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MitBud.Models;
+using MitBud.DAL;
+
+namespace MitBud.Providers
+{
+    public class TaskProvider
+    {
+
+
+        //Save Tasks to the database to the Tasks table
+        public static void SaveTask(TaskViewModel TaskViewModel, string userId)
+        {
+            MitBudDBEntities db = new MitBudDBEntities();
+            Task Task = new Task();
+            Task.Title = TaskViewModel.Title;
+            Task.Client_id = userId;
+            Task.Description = TaskViewModel.Description;
+            Task.Category = TaskViewModel.Category;
+            Task.ClientName = TaskViewModel.ClientName;
+            Task.ClientAddress = TaskViewModel.ClientName;
+            Task.ClientPostCode = TaskViewModel.ClientPostCode;
+            Task.ClientTelephone = TaskViewModel.ClientTelephone;
+            Task.ClientEmail = TaskViewModel.ClientEmail;
+            Task.WhoAreYou = TaskViewModel.WhoAreYou;
+            Task.TaskCost = TaskViewModel.TaskCost;
+            db.Tasks.Add(Task);
+            db.SaveChanges();
+
+         
+        }
+
+    }
+}
